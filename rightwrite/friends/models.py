8dd3a69b2a949e6db3ebf3088758e754e1820504
Monique Murphy <momurphy@austin.utexas.edu>
2019-01-02 15:59:03 -0600

    figuring out Friend model

--- rightwrite/friends/models.py
+++ rightwrite/friends/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+class Friend(models.Model):
+    base_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='friend_self')
+    friend = models.ForeignKey('User', on_delete=models.CASCADE, related_name='friend_friend')
